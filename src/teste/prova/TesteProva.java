726904ccef8ffbbd3622bc6f9a9a4ed99a2c642a
marcuss83 <marcus_vs_ferreira@estudante.sc.senai.br>
2021-05-05 09:34:24 -0300

    camiseta confortavel

---
+++ src/teste/prova/TesteProva.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste.prova;
+
+import java.util.Scanner;
+ 
+public class TesteProva {
+
+   
+    public static void main(String[] args) {
+         int num1, num2, soma;
+        Scanner ent = new Scanner(System.in);
+         
+        System.out.println ("Digite o primeiro numero");
+        num1 = ent.nextInt(); 
+        System.out.println ("Digite o segundo numero");
+        num2 = ent.nextInt();
+        
+        soma = (num1 + num2);
+        
+        System.out.println ("A soma dos 2 numeros é: " + soma);
+        
+        //codigo BY MARCUS :) GENIO DO TI, me da nota pfv professor
+        
+        
+                
+    }
+            
+    
+}
